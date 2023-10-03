4c7e010fa9210f1c03f29dd98d6e0a3f3f7989fd
katgur <mitsutanaka29@gmail.com>
2023-10-03 21:00:29 +0300

    fixed nav link

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -2,6 +2,7 @@ import { getUser, logout } from "../reducers/authReducer"
 import { useSelector } from "react-redux"
 import { useDispatch } from "react-redux"
 import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap'
+import { Link } from "react-router-dom"
 
 function Navigation() {
     const user = useSelector(getUser)
@@ -22,8 +23,8 @@ function Navigation() {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="/users">users</Nav.Link>
-                        <Nav.Link href='/'>blogs</Nav.Link>
+                        <Nav.Link as={Link} to="/users">users</Nav.Link>
+                        <Nav.Link as={Link} to='/'>blogs</Nav.Link>
                         <NavDropdown title={`${user.name} logged in`} id="basic-nav-dropdown">
                             <NavDropdown.Item onClick={handleLogout}>Log out</NavDropdown.Item>
                         </NavDropdown>
